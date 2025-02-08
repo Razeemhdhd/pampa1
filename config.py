fa3d8bca33aea0ab80969d64b13e73447c6bf841
Razeemhdhd <bhatroshni050@gmail.com>
2025-02-08 19:10:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY @SomsPvt
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7481858892:AAHZb-H8kM_6a_nhWH45ZPL3SYb9AVBAuqk")
+BOT_TOKEN=("7826585345:AAETnXbX8BG5Uoj7OVKWik1qqDY-siTD008")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@BLURYFAXE")
+OWNER_USERNAME=("@UknowJoHaN")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("7221087191")
+ADMIN_IDS=("1225401466,7702119573")
 
 
 
